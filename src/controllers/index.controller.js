e555f502da387634da56b3fff07d89002924d2a0
Sergio Díaz <codediaz@outlook.es>
2021-08-21 07:04:42 -0500

    Nodemon, routes and controller edited, and port changed

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,10 @@
+
+
+const getUsers = (req,res) => {
+    res.send('users')
+
+}
+
+module.exports = {
+    getUsers
+}
---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended : false} ))
+
+//Routes
+app.use(require('./routes/index'))
+
+port = 4000
+app.listen(port)
+console.log('El servidor está en el puerto ' + port)
